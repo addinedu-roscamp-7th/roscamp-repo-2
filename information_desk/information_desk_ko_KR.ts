4eb3ee39ddc3f61e5ac531ad5ba9019358612818
Park Byeong Gyu <park0112@gmail.com>
2025-10-29 17:22:30 +0900

    [ENVIR/info_desk]: 배포를 위한 CMakeLisk.txt 설정 변경

--- information_desk/information_desk_ko_KR.ts
+++ information_desk/information_desk_ko_KR.ts
@@ -1,3 +1,176 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko_KR"></TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>BookInfoLocation</name>
+    <message>
+        <location filename="bookinfolocation.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookinfolocation.ui" line="53"/>
+        <source>도서 위치</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BookLoan</name>
+    <message>
+        <location filename="bookloan.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="32"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="35"/>
+        <source>회원증을 스캔해주세요!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="76"/>
+        <source>회원증 스캔    |</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="98"/>
+        <source>스캔 대기 중</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="123"/>
+        <source>취소</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="164"/>
+        <source>제목:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="186"/>
+        <source>저자:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="208"/>
+        <source>출판사:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="230"/>
+        <source>표준번호:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bookloan.ui" line="244"/>
+        <source>임시버튼</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BookSearchDialog</name>
+    <message>
+        <location filename="booksearchdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="booksearchdialog.ui" line="83"/>
+        <source>검색</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DobbyLoan</name>
+    <message>
+        <location filename="dobbyloan.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="47"/>
+        <source>이용해주셔서 감사합니다!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="66"/>
+        <source>보관함에서</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="85"/>
+        <source> 뒤에 책을 수령해주세요!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="107"/>
+        <location filename="dobbyloan.ui" line="224"/>
+        <source>1번</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="129"/>
+        <source>10분</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="154"/>
+        <source>초기화면</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="178"/>
+        <source>4번</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="201"/>
+        <source>2번</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dobbyloan.ui" line="247"/>
+        <source>3번</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfoDeskMain</name>
+    <message>
+        <location filename="infodeskmain.ui" line="14"/>
+        <source>InfoDeskMain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="infodeskmain.ui" line="35"/>
+        <source>도서 조회</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="infodeskmain.ui" line="56"/>
+        <source>도서 대출</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="infodeskmain.ui" line="77"/>
+        <source>보관 도서 
+ 가져가기</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="infodeskmain.ui" line="99"/>
+        <source>좌석 예약</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="infodeskmain.ui" line="120"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:20pt; font-weight:700;&quot;&gt;스마트 도서관에 &lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:20pt; font-weight:700;&quot;&gt;오신 것을 환영합니다.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
